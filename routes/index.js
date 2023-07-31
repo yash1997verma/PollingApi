f5cea121072c5e5c2ef5fefafd141e97dfe7091e
Yash Verma <yash1997verma@gmail.com>
2023-07-31 11:11:39 +0530

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const  router  = express.Router();
 
+router.get('/', (req, res)=>{
+    res.json('Welcome to Polling Api');
+})
 router.use('/questions', require('./question') );
 router.use('/options', require('./option'));
 
